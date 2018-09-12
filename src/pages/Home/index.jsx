3993663c7fa4af569bbf4524c4dfd73b8ceab470
unknown <konrad.leon.toldy@gmail.com>
2018-09-12 10:29:04 +0200

    bok

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 // import './index.css';
 import styles from './index.css';
-
+import { Button } from 'storybook-project/dist/';
 export default class Home extends React.Component {
   render() {
     return (
       <div className="home">
         <h1> Hello World! </h1>
         <h4>Links:</h4>
+         <Button text="Press me!"></Button>
         <div className={styles.customHome} >
           <Link href="a" to="/somethingNotFound"> Go to Not Found page! </Link>
         </div>
